9613d63758bcf17c83e4c9b926fd1429ca02e5d8
Teodor Lagerqvist <git@teodor.se>
2021-12-18 22:25:21 +0100

    Export ColorSensorRGBData type

--- src/lib/devices/DistanceColorSensor.ts
+++ src/lib/devices/DistanceColorSensor.ts
@@ -1,6 +1,6 @@
-import { Port, DeviceType, SubCommands, ICommandOptions } from "../types";
-import { Device } from "../Device";
-import { Hub } from "../Hub";
+import { Port, DeviceType, SubCommands, ICommandOptions } from '../types';
+import { Device } from '../Device';
+import { Hub } from '../Hub';
 
 // IR Blaster
 // To initialize the Color Sensor in the proper mode/frequency/color/whatever: 0a004100070100000001
@@ -17,12 +17,13 @@ export enum DistanceColorModes {
   AMBIENT = 0x04, // Blue light.  Ambient light in percent
   LED_COLOR = 0x05, // starts black. Can set color by setColor()
   RGB = 0x06, // RGB....  Returns R G and B
-  IR_TRANSMITTER = 0x07, // // black. 
+  IR_TRANSMITTER = 0x07, // // black.
   COLOR_DISTANCE = 0x08, // RGB, both color and distance data. and close distance?
   DEBUG = 0x09,
-  CALIBRATION = 0x0A
+  CALIBRATION = 0x0a
 }
 
+export type DistanceColorRGBData = { r: number; g: number; b: number };
 
 export enum LedColor {
   OFF = 0x00,
@@ -34,27 +35,31 @@ export enum LedColor {
 
 export class DistanceColorSensor extends Device {
   constructor(hub: Hub, port: Port) {
-    super(hub, port, DeviceType.DISTANCE_COLOR_SENSOR)
+    super(hub, port, DeviceType.DISTANCE_COLOR_SENSOR);
   }
 
   public setColor(color: LedColor, options?: ICommandOptions) {
-    const data = Buffer.from([SubCommands.WRITE_DIRECT_MODE_DATA, DistanceColorModes.LED_COLOR, color]);
-    this.sendCommand(data, options)
+    const data = Buffer.from([
+      SubCommands.WRITE_DIRECT_MODE_DATA,
+      DistanceColorModes.LED_COLOR,
+      color
+    ]);
+    this.sendCommand(data, options);
   }
-  
+
   public parseSensorReading(buffer: Buffer, mode: DistanceColorModes) {
-    switch(mode) {
-      case DistanceColorModes.LED_COLOR: 
-        return {color: buffer.readUInt8(4) as LedColor}
-      case DistanceColorModes.COLOR: 
-        return { color: buffer.readUInt8(4) }
-      case DistanceColorModes.PROXIMITY: 
-        return { distance: buffer.readUInt8(4) }
+    switch (mode) {
+      case DistanceColorModes.LED_COLOR:
+        return { color: buffer.readUInt8(4) as LedColor };
+      case DistanceColorModes.COLOR:
+        return { color: buffer.readUInt8(4) };
+      case DistanceColorModes.PROXIMITY:
+        return { distance: buffer.readUInt8(4) };
       case DistanceColorModes.COLOR_DISTANCE:
-        const color = buffer.readUInt8(4)
-        const distance = buffer.readUInt8(5)
-        const distance2 = buffer.readUInt8(7)
-        let millimeters = 0
+        const color = buffer.readUInt8(4);
+        const distance = buffer.readUInt8(5);
+        const distance2 = buffer.readUInt8(7);
+        let millimeters = 0;
         if (distance2 > 0) {
           millimeters = Math.floor(20 - distance2 * 2.85);
         } else if (distance > 9) {
@@ -62,18 +67,18 @@ export class DistanceColorSensor extends Device {
         } else {
           millimeters = Math.floor(20 + distance * 18);
         }
-        return {color,distance,millimeters} 
-      case DistanceColorModes.RGB: 
-        return {
+        return { color, distance, millimeters };
+      case DistanceColorModes.RGB:
+        const rgb: DistanceColorRGBData = {
           r: buffer.readUInt8(4),
           g: buffer.readUInt8(6),
           b: buffer.readUInt8(8)
-        }
+        };
+        return rgb;
       case DistanceColorModes.COUNTING:
-        return {counter: buffer.readInt32LE(4)}
+        return { counter: buffer.readInt32LE(4) };
       default:
-        return {}
+        return {};
     }
   }
-
 }
